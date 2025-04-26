8ac874f91a4420f05b821a7d04c790a493c8b70d
Alex <steblyakov.aleksandr@gmail.com>
2025-04-26 18:22:16 +0300

    Finishedn general validation using supertest

--- __tests__/videos.e2e.tests.ts
+++ __tests__/videos.e2e.tests.ts
@@ -57,6 +57,30 @@ describe("Overall API tests", () => {
     await request(app).put(`/videos/1`).send(testVideoUpdate).expect(HttpStatus.NoContent);
 
     const testResponse = await request(app).get(`/videos/1`).expect(HttpStatus.Ok);
-    expect(testResponse.body[0].title).toBe("UpdatedTestMovie01");
+
+    const testAdditionalResponse = await request(app).get(`/videos/`).expect(HttpStatus.Ok);
+    expect(testAdditionalResponse.body[0].title).toBe("UpdatedTestMovie01");
+  });
+
+  it("DELETE /videos/1 - should return 204 no content and successfully pass following checks", async () => {
+    const testAnotherVideoRecord: Video = {
+      id: 2,
+      title: "TestMovie02",
+      author: "TestAuthor02",
+      canBeDownloaded: false,
+      minAgeRestriction: 12,
+      createdAt: new Date(),
+      publicationDate: CreateDefaultDate(), //new Date(new Date().getDate() + 1),
+      availableResolutions: [Resolution.P240, Resolution.P144],
+    };
+
+    await request(app).post(`/videos/`).send(testAnotherVideoRecord).expect(HttpStatus.Created);
+
+    await request(app).delete(`/videos/1`).expect(HttpStatus.NoContent);
+
+    const testResponse = await request(app).get(`/videos/`).expect(HttpStatus.Ok);
+    expect(testResponse.body[0].id).toBe(2);
+
+    const testAdditionalResponse = await request(app).get(`/videos/1`).expect(HttpStatus.NotFound);
   });
 });
